b19e7e4f92f4b13aca59c8f5a60a46396dee0e11
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-11-01 11:48:16 +0000

    created container

---
+++ restaurant-client/src/containers/RestaurantBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class RestaurantBox extends Component {
+
+
+  render() {
+    return (
+      <h2>This is the RestaurantBox</h2>
+    );
+  }
+}
+
+export default RestaurantBox;
