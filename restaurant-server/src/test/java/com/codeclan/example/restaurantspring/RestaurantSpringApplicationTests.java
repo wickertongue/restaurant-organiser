29f4500ea4ff2e8a231cc20e6798f35fdf7ff761
Ted Osuch <ted@osuch.com>
2019-11-01 10:00:47 +0000

    added spring files

---
+++ restaurant-server/src/test/java/com/codeclan/example/restaurantspring/RestaurantSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.restaurantspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
