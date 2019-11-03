657364fb018fde3fd0686922fe218df514b3a634
Ross Mckay <ross.mckay1975@gmail.com>
2019-11-03 22:51:30 +0000

    add css to 5table layout

--- restaurant-client/src/components/TableLayout.js
+++ restaurant-client/src/components/TableLayout.js
@@ -17,8 +17,11 @@ class TableLayout extends Component {
                 <div className="table1">Table 1 </div>
                 <div className="table2">Table 2</div>
                 <div className="table3">Table 3</div>
+                <div className="table4">Table 4</div>
+                <div className="table5">Table 5</div>
             </div>
-        )
+            
+             )
     }
 
 }
