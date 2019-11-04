cd758410f7c25be0a3acb7462a9a9883f22aa496
hayden <haydenwb@gmail.com>
2019-11-04 10:57:13 +0000

    linking data into tablelayout

--- restaurant-client/src/containers/RestaurantBox.js
+++ restaurant-client/src/containers/RestaurantBox.js
@@ -52,7 +52,7 @@ class RestaurantBox extends Component {
     return (
       <div className="booking-list">
         <h2>This is the RestaurantBox</h2>
-        <TableLayout/>
+        <TableLayout data={this.state.bookings}/>
         <BookingList data={this.state.bookings}/>
         <BookingForm />
       </div>
