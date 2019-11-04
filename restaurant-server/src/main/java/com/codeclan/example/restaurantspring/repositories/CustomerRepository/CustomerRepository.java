9a3c47d39e36b1efa76fc5684be8d428a6915a57
hayden <haydenwb@gmail.com>
2019-11-04 16:21:46 +0000

    added comments to customerrepository

--- restaurant-server/src/main/java/com/codeclan/example/restaurantspring/repositories/CustomerRepository/CustomerRepository.java
+++ restaurant-server/src/main/java/com/codeclan/example/restaurantspring/repositories/CustomerRepository/CustomerRepository.java
@@ -3,5 +3,13 @@ package com.codeclan.example.restaurantspring.repositories.CustomerRepository;
 import com.codeclan.example.restaurantspring.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long>, CustomerRepositoryCustom {
+
+    // List<Customer> findAllOrderByNumberOfBookingsDesc();
+
+    // find number of all bookings by customer
+    // find all bookings by customer
+    // FindByBookingsContainsOrderByNumberOfBookingsDesc
 }
