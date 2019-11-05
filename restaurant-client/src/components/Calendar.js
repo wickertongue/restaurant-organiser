6a2b141d7df33e9770a51e7d56f59d830ae840d3
Ross Mckay <ross.mckay1975@gmail.com>
2019-11-05 14:15:03 +0000

    date format changed

--- restaurant-client/src/components/Calendar.js
+++ restaurant-client/src/components/Calendar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import DatePicker from "react-datepicker";
  
 import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import es from 'date-fns/locale/es';
+registerLocale('es', es);
  
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
@@ -22,6 +25,7 @@ class Calendar extends React.Component {
       <DatePicker
         selected={this.state.startDate}
         onChange={this.handleChange}
+        dateFormat="dd/MM/yyyy"
       />
     );
   }
